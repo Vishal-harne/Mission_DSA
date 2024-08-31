150e82c49b1b1d91b70ab0ee331a989f915d18bd
Vishal-harne <123869546+Vishal-harne@users.noreply.github.com>
2024-08-31 18:53:10 +0530

    Create reversedstrign.cpp

---
+++ reversedstrign.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str="";
+        for(char ch:s){
+            if(isalnum(ch)){
+                str+=tolower(ch);
+            }
+        }
+        string reversed=str;
+        reverse(str.begin(),str.end());
+        return str==reversed;
+    }
+};
+//l1  you cant directlt compare the result of reverse funciton becuase it return void valeu
+//return str==reverse(str.begin(),str.end()
+//read the error and understand the reason behind this and write it down
