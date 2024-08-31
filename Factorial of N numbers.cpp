11f9df1a387d3f769b58049e5f46e76406dfcb95
Vishal-harne <123869546+Vishal-harne@users.noreply.github.com>
2024-08-31 18:37:17 +0530

    Update Factorial of N numbers.cpp

--- Factorial of N numbers.cpp
+++ Factorial of N numbers.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+  public:
+ 
+    vector<long long> factorialNumbers(long long n) {
+        vector<long long>ans;
+       long long result=1;
+     for(int i=1;i<=n;i++){
+         result*=i;
+         if(result>n){
+             return ans;
+         }
+         ans.push_back(result);
+     }
+     return ans;
+    }
+};
+
