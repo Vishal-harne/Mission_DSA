6e50eb6e2022366941562b57203d66cdf3115be7
Vishal-harne <123869546+Vishal-harne@users.noreply.github.com>
2024-09-23 18:15:51 +0530

    Create set zeros.cpp

---
+++ set zeros.cpp
@@ -0,0 +1,60 @@
+//to get more optio=mise solution we will going use the first row and column as marker 
+//it will save our o(m+n) space and do in place replacement to get better solution
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        bool firstrowzero=false;
+        bool firstcolzero=false;
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]==0){
+                firstcolzero=true;
+                break;
+            }
+        }
+           for(int i=0;i<m;i++){
+            if(matrix[0][i]==0){
+                firstrowzero=true;
+                break;
+            }
+        }
+        //first row and first col use as an indentify that which row and col has to be zero
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+               if(matrix[i][j]==0){
+                   matrix[0][j]=0;
+                   matrix[i][0]=0;
+               }
+            }
+        }
+        // now make changes in matrix and make  row and col as zero acc to first row and col
+        for(int i=1;i<n;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<m;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            if(matrix[0][i]==0){
+                for(int j=0;j<n;j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+
+        // now check for first row and col 
+        if(firstrowzero){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(firstcolzero){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+};
