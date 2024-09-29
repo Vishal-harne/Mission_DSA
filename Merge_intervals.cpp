460f970544904db8e115dc440e6b241959ef1463
Vishal-harne <123869546+Vishal-harne@users.noreply.github.com>
2024-09-29 14:14:02 +0530

    Update Merge_intervals.cpp

--- Merge_intervals.cpp
+++ Merge_intervals.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return {};
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>> merged;
-        merged.push_back(intervals[0]);
-        for (int i = 1; i < intervals.size(); i++) {
-            vector<int>& last = merged.back();//we dont need to update it every time either it will go inside or stay till it not overlapp with ohter
-            if (last[1] >= intervals[i][0]) {
-                last[1] = max(last[1], intervals[i][1]);
-            } else {
-                merged.push_back(intervals[i]);
-            }
-        }
+    int n = intervals.size();
+  
+    vector<vector<int>> v;
 
-        return merged;
+    sort(intervals.begin(), intervals.end());
+
+    vector<int> v1 = intervals[0];
+    
+    for (int i = 1; i < n; i++) {
+        if (v1[1] >= intervals[i][0]) {
+            v1[1] = max(v1[1], intervals[i][1]);
+        }
+        else {
+            v.push_back(v1);
+            v1 = intervals[i];
+        }
     }
-};
+    
+    v.push_back(v1); // Push the last merged interval
+    
+    return v;
+}};
+
+
