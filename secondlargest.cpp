9076afd8da583affc424d7d669a445dbdb6483a3
Vishal-harne <123869546+Vishal-harne@users.noreply.github.com>
2024-11-11 17:00:05 +0530

    Update secondlargest.cpp

--- secondlargest.cpp
+++ secondlargest.cpp
@@ -2,11 +2,14 @@ class Solution {
   public:
     
     int print2largest(vector<int> &arr) {
+      // largest and larger make 2 varieble
        int largest=INT_MIN;
         int secondlar=INT_MIN;
+      //check it it is more than 2 varieble if not -1
         if(arr.size()<2){
             return -1;
         }
+      //
      for(int i=0;i<arr.size();i++){
          if(arr[i]>largest){
              secondlar=largest;
