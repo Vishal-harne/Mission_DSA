70e184d9d651b02a935757bbf25741a0c65394ee
Vishal <123869546+Vishal-harne@users.noreply.github.com>
2025-02-25 10:19:20 +0530

    Create QuickSort.cpp

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,59 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<int> & ans,int low,int high){
+    int pivot=ans[low];2
+    int i=low;2
+    int j=high;5
+    while(i<j){
+        while(ans[i]<=pivot &&  i<=high-1){
+            i++;
+        }
+        while(ans[j]>pivot  &&  j>=low+1){
+            j--;
+        }
+        if(i<j){
+            swap(ans[i],ans[j]);
+        }
+    }
+    swap(ans[low],ans[j]);
+    int n=ans.size();
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return j;
+}
+
+void QuickSort(vector<int> & ans,int low,int high){
+    int partiti=0;
+    if(low<high){
+      int pivotindex=partition(ans,low,high);
+      cout<<" pivotindex" <<ans[pivotindex]<<" "<<endl;
+           cout<<"left hitted"<<endl;
+       QuickSort(ans,low,pivotindex-1);
+        cout<<"right hitted"<<endl;
+      QuickSort(ans,pivotindex+1,high);
+    }
+}
+
+int main() {
+  cout<<"Enter the value fo N: ";
+  int n;
+  cin>>n;
+  vector<int>ans;
+  for(int i=0;i<n;i++){
+      int a;
+      cin>>a;
+      ans.push_back(a);
+  }
+  
+  QuickSort(ans,0,n-1);
+  for(int i=0;i<n;i++){
+     cout<<ans[i]<<" ";
+  }
+  cout<<endl;
+
+    return 0;
+}
