bf8356e9cb791fc9eff7b85d440501bf096dcae2
Vishal <123869546+Vishal-harne@users.noreply.github.com>
2025-05-17 22:39:37 +0530

    Update 3Sum.cpp

--- AcciojobQustionBank/RSL/3Sum.cpp
+++ AcciojobQustionBank/RSL/3Sum.cpp
@@ -17,3 +17,26 @@ class Solution {
       return ans1;
     }
 };
+
+
+//optimisedapproch
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &ans) {
+        int n=ans.size();
+       unordered_map<int,vector<int>>map;
+       vector<vector<int>>ans1;
+       for(int j=0;j<n-1;j++){
+           for(int k=j+1;k<n;k++){
+               int val=-1*(ans[j]+ans[k]);
+               if(map.find(val)!=map.end()){
+                   for (auto i: map[val]) {
+                    ans1.push_back({i, j, k});
+                }
+               }
+           }
+           map[ans[j]].push_back(j);
+       }
+       return ans1;
+    }
+};
