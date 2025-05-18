c37607bea4d9ac66b3e55da182f9dec13ce329b5
Vishal <123869546+Vishal-harne@users.noreply.github.com>
2025-05-18 07:30:01 +0530

    Create APointOnLine.cpp

---
+++ AcciojobQustionBank/RSL/APointOnLine.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  int n,d;
+  cin>>n>>d;
+  vector<int>ans(n);
+  for(int i=0;i<n;i++){
+      cin>>ans[i];
+  }
+  
+  int cnt=0;
+  for(int i=0;i<n-2;i++){
+      for(int j=i+1;j<n-1;j++){
+          for(int k=j+1;k<n;k++){
+              if((ans[k]-ans[i])<=d){
+                  cnt++;
+              }
+          }
+      }
+  }
+ cout<<cnt<<endl;
+ return 0;
+}
