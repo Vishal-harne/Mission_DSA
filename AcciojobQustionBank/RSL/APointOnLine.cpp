e322e73ac78209f1364a177949f6065f5ba9e835
Vishal <123869546+Vishal-harne@users.noreply.github.com>
2025-05-18 08:15:33 +0530

    Update APointOnLine.cpp

--- AcciojobQustionBank/RSL/APointOnLine.cpp
+++ AcciojobQustionBank/RSL/APointOnLine.cpp
@@ -23,3 +23,44 @@ int main() {
  cout<<cnt<<endl;
  return 0;
 }
+
+
+//optimised solution o(n2)
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    long long d;
+    cin >> n >> d;
+
+    vector<long long> x(n);
+    
+    for (int i = 0; i < n; ++i)
+        cin >> x[i];
+        
+        
+
+    long long result = 0;
+      
+    int j = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        while (j < n && x[j] - x[i] <= d)
+            j++;
+        long long cnt = j - i - 1;
+        if (cnt >= 2)
+            result += cnt * (cnt - 1) / 2;
+            cout<<cnt<<endl;
+            
+    }
+
+    cout << result << endl;
+    return 0;
+}
+
+
