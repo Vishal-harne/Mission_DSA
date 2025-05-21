b491a01767fa1172fa9055aaccb6347ce471abf8
Vishal <123869546+Vishal-harne@users.noreply.github.com>
2025-05-21 15:49:10 +0530

    Update MoveAllZerosToEnd.cpp

--- Array/Easy/MoveAllZerosToEnd.cpp
+++ Array/Easy/MoveAllZerosToEnd.cpp
@@ -15,3 +15,22 @@ class Solution {
         
     }
 };
+
+
+
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+         int n=arr.size();
+        int ind=0;
+        
+        for(int i=0;i<n;i++){
+           if(arr[i]!=0){
+               swap(arr[ind],arr[i]);
+               ind++;
+           }
+        }
+      
+        
+    }
+};
