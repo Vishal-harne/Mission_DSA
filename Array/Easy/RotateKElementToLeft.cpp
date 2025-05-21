7da4b703364b755d9e8f3215f7868259140a23bf
Vishal <123869546+Vishal-harne@users.noreply.github.com>
2025-05-21 15:06:35 +0530

    Update RotateKElementToLeft.cpp

--- Array/Easy/RotateKElementToLeft.cpp
+++ Array/Easy/RotateKElementToLeft.cpp
@@ -17,3 +17,27 @@ class Solution {
        }
     }
 };
+
+
+//optimised Aproach
+class Solution {
+  public:
+
+    void rotateArr(vector<int>& arr, int d) {
+       int n=arr.size();
+       if(d>n){
+           d=d%n;
+       }
+       vector<int>ans;
+       for(int i=d;i<n;i++){
+           ans.push_back(arr[i]);
+       }
+       for(int j=0;j<d;j++){
+           ans.push_back(arr[j]);
+       }
+       for(int i=0;i<ans.size();i++){
+          arr[i]=ans[i];
+       }
+       
+    }
+};
