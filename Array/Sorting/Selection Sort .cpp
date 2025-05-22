cedf15934c69177d52af89f27018917166062a3d
Vishal <123869546+Vishal-harne@users.noreply.github.com>
2025-05-22 07:11:42 +0530

    Create Selection Sort .cpp

---
+++ Array/Sorting/Selection Sort .cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+      int n=arr.size();
+      for(int i=0;i<n;i++){
+          int mini=i;
+          for(int j=i;j<n;j++){
+             if(arr[j]<arr[mini]){
+                 mini=j;
+             }
+          }
+          swap(arr[i],arr[mini]);
+      }
+        
+    }
+};
